047f3b5f6f79d65142afce21031480af843bc033
Rijoan Fardous <rijoanfardous101@gmail.com>
2024-11-29 16:28:30 +0600

    Real-time chat feature added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
+import { useThemeStore } from "./store/useThemeStore.js";
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin"></Loader>
+      </div>
+    );
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/settings"
+          element={authUser ? <SettingsPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
